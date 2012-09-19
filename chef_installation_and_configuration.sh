a757dfd68c3d42eb527e09b76dcf31b1510f3f77
Santosh Kumar <sk21886-mygit@yahoo.co.in>
2012-09-19 23:46:15 +0530

    method added to configure chef-client on local system

--- chef_installation_and_configuration.sh
+++ chef_installation_and_configuration.sh
@@ -68,6 +68,7 @@ function install_chef_server
 
 function configure_knife_on_chef_server
 {
+	#run this chef server
 	#shell array declaration
 	servers=( 'chef-server-webui' 'chef-server (api)' 'chef-expander' 'chef/solr' 'rabbitmq' 'couchdb' )
 	#array size: ${#servers[@]}
@@ -106,11 +107,60 @@ function configure_knife_on_chef_server
 	echo "for configuration help pease visit: http://wiki.opscode.com/display/chef/Installing+Chef+Server+on+Debian+or+Ubuntu+using+Packages" >&2
 	echo "Please enter the location of the existing admin client's private key: [/etc/chef/webui.pem] .chef/webui.pem" >&2
 	echo "Please enter the location of the validation key: [/etc/chef/validation.pem] .chef/validation.pem" >&2
-	knife configure -i
+	sudo knife configure -i
 	if [[ $? == 0 ]];then
 		echo "server knife configuration done." >&2
+		echo "YOU CAN RUN KNIFE COMMAND TO CHECK IF CONFIGURED: knife client list" >&2
 	else
 		echo "server knife configuration failed." >&2
+	fi
+}
+
+function configure_knife_client_on_mylaptop
+{
+	chef_server=$1
+	user_name=$2
+	echo "creating chef client on server" >&2
+	ssh $chef_server knife client $user_name -d -a -f /tmp/$user_name.pem
+	if [[ $? != 0 ]];then
+		echo "failed to create client:$user_name on chef server" >&2
+		exit 1
+	else
+		echo "client created:$user_name on chef server, the private key is stored in file $chef_server:/tmp/$user_name.pem" >&2
+		echo "testing client:" >&2
+		ssh $chef_server knife client show $user_name
+	fi
+
+	echo "creating chef dir ~/.chef" >&2
+	mkdir -p ~/.chef
+	if [[ $? != 0 ]];then
+		echo "failed to create chef dir" >&2
+		exit 1
+	fi
+
+	echo "copying the key just created on server" >&2
+	scp $chef_server:/tmp/$user_name.pem ~/.chef/$user_name.pem
+	if [[ $? != 0 ]];then
+		echo "failed to copy client key from server" >&2
+		exit 1
+	fi
+
+	echo "installing chef-client, ruby gem" >&2
+	gem install chef
+	if [[ $? != 0 ]];then
+		echo "failed to install ruby gem: chef-client" >&2
+		exit 1
+	fi
+
+	echo "When prompting for chef-server url, please enter the chef server URL: [http://localhost:4000] http://chef-server.example.com:4000" >&2
+	echo "When prompting for uername or client name please specify the username just created or any existing user" >&2
+	
+	knife configure
+	if [[ $? == 0 ]];then
+		echo "chef-client configuration done." >&2
+		echo "YOU CAN RUN KNIFE COMMAND TO CHECK IF CONFIGURED: knife client list" >&2
+	else
+		echo "chef-client configuration failed." >&2
 	fi
 }
 
@@ -118,6 +168,7 @@ function usage
 {
 	echo "$0 install_chef_server"
 	echo "$0 configure_knife_on_chef_server"
+	echo "$0 configure_knife_client_on_mylaptop <chef_server_address> <new_chef_user_name>"
 }
 
 if [[ $1 == "install_chef_server" ]];then
@@ -126,6 +177,11 @@ if [[ $1 == "install_chef_server" ]];then
 	install_chef_server
 elif [[ $1 == "configure_knife_on_chef_server" ]];then
 	configure_knife_on_chef_server
+elif [[ $# == 4 ]] && [[ $1 == "configure_knife_client_on_mylaptop" ]];then
+	#use this if yourlaptop itself is not the server.
+	#When working with chef, you will spend a lot of time editing recipes and other files, and you'll find it much more convenient to edit them on your laptop/desktop (your management workstation), where you have your editor configured just to your liking. To facilitate this mode of working, we recommend you create a knife client to use knife on your development machine.
+	#Make sure you've configured knife on your chef server before proceeding with this step.
+	configure_knife_client_on_mylaptop $2 $3
 else
 	usage
 fi
