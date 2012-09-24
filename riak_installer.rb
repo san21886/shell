7a420049e58f1ee4d65f3d148a2fc7f766ed5524
Santosh Kumar <sk21886-mygit@yahoo.co.in>
2012-09-24 15:51:24 +0530

    script to install and setup riak cluster

---
+++ riak_installer.rb
@@ -0,0 +1,59 @@
+ #!/bin/bash
+
+download_dir="$HOME/riak"
+
+if [[ ! -d $download_dir ]]; then
+	mkdir -p $download_dir
+fi
+
+function install_erlang
+{
+	sudo apt-get install build-essential libncurses5-dev openssl libssl-dev
+	wget http://erlang.org/download/otp_src_R14B03.tar.gz
+	tar zxvf otp_src_R14B03.tar.gz
+	rsync -avz otp_src_R14B03 $download_dir
+	(cd $download_dir/otp_src_R14B03 && ./configure && make && sudo make install)
+}
+
+#http://wiki.basho.com/Building-a-Development-Environment.html
+function install_riak
+{
+	sudo apt-get install build-essential libc6-dev-i386
+	wget 'http://downloads.basho.com.s3-website-us-east-1.amazonaws.com/riak/CURRENT/riak-1.2.0.tar.gz'
+	tar zxvf "riak-1.2.0.tar.gz"
+	rsync -avz "riak-1.2.0" $download_dir
+	(cd "$download_dir/riak-1.2.0" && make rel)
+}
+
+function start_riak_and_create_cluster
+{
+	#starting nodes
+	for node in dev1 dev2 dev3 dev4
+	do
+		(cd "$download_dir/riak-1.2.0/dev" && $node/bin/riak start)
+	done
+
+	#making cluster
+	for node in dev2 dev3 dev4
+	do
+		(cd "$download_dir/riak-1.2.0/dev" && $node/bin/riak-admin join "dev1@127.0.0.1")
+	done
+}
+
+function usage
+{
+	echo "$0 install_erlang" >&2
+	echo "$0 install_riak" >&2
+	echo "$0 start_riak_and_create_cluster" >&2
+}
+
+if [[ $1 == "install_erlang" ]]; then
+	install_erlang
+elif [[ $1 == "install_riak" ]]; then
+	install_riak
+elif [[ $1 == "start_riak_and_create_cluster" ]]; then
+	start_riak_and_create_cluster
+else
+	usage
+fi
+
